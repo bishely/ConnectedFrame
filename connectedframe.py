33dfaa357949a5d88be1bceee45da8e4b7a41409
bishely <bishely@gmail.com>
2017-09-09 00:33:14 +0100

    Just getting to grips
    Just chucking stuff into the code and figuring out how/where I want the MQTT messages to trigger the dropbox download.

--- connectedframe.py
+++ connectedframe.py
@@ -4,7 +4,10 @@ from Tkinter import *
 from os import putenv, getenv, system
 from PIL import Image, ImageTk 
 from glob import glob
+from Adafruit_IO import MQTTClient
 
+ADAFRUIT_IO_KEY = getenv("ADAFRUIT_KEY")
+ADAFRUIT_IO_USERNAME = getenv("ADAFRUIT_USER")
 dropbox_link = getenv("DROPBOX_LINK")
 download_interval = int(getenv("DOWNLOAD_INTERVAL_HOURS")) * 60 * 60 * 1000
 carousel_interval = int(getenv("CAROUSEL_INTERVAL_SECONDS")) * 1000
@@ -16,6 +19,36 @@ carrousel_status = True
 image_index = 0
 image_list = []
 initial_init = True
+
+# Define callback functions which will be called when certain events happen.
+def connected(client):
+    # Connected function will be called when the client is connected to Adafruit IO.
+    # This is a good place to subscribe to feed changes.  The client parameter
+    # passed to this function is the Adafruit IO MQTT client so you can make
+    # calls against it easily.
+    print('Connected to Adafruit IO!  Listening for DemoFeed changes...')
+    # Subscribe to changes on a feed named DemoFeed.
+    client.subscribe('DemoFeed')
+
+def disconnected(client):
+    # Disconnected function will be called when the client disconnects.
+    print('Disconnected from Adafruit IO!')
+    sys.exit(1)
+
+def message(client, feed_id, payload):
+    # Message function will be called when a subscribed feed has a new value.
+    # The feed_id parameter identifies the feed, and the payload parameter has
+    # the new value.
+    print('Feed {0} received new value: {1}'.format(feed_id, payload))
+
+
+# Create an MQTT client instance.
+client = MQTTClient(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
+
+# Setup the callback functions defined above.
+client.on_connect    = connected
+client.on_disconnect = disconnected
+client.on_message    = message
 
 def download_images(url):
 	archive = base_path + "temp.zip"
@@ -110,7 +143,7 @@ def initialize():
 		initial_init = False
 		root.after(1000, initialize)
 	else:
-		root.after(download_interval, initialize)
+		root.after(download_interval, initialize) # Change this to only run initialise if MQTT message arrives
 
 def send_event():
 	img = ImageTk.PhotoImage(Image.open("/usr/src/app/icons/liked.png"))
@@ -121,6 +154,17 @@ def send_event():
 
 	system(command)
 
+# Connect to the Adafruit IO server.
+client.connect()
+
+# Now the program needs to use a client loop function to ensure messages are
+# sent and received.  There are a few options for driving the message loop,
+# depending on what your program needs to do.
+
+# The first option is to run a thread in the background so you can continue
+# doing things in your program.
+client.loop_background()
+	
 root = Tk()
 root.title('Connected Frame')
 root.geometry('{}x{}'.format(800, 480))
